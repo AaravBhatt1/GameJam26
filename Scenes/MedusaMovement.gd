dea2fd404f9d839dfbc2e09ce85cdabcba84d5ca
Ethan Smith <ethanssmith1506@gmail.com>
2026-02-27 21:45:02 +0000

    Medusa Movement

--- Scenes/MedusaMovement.gd
+++ Scenes/MedusaMovement.gd
@@ -1,8 +1,13 @@
 extends Area2D
 
+var TILE_SIZE = 32
+var ANIMATION_SPEED = 3
+
 enum Direction {UP, DOWN, LEFT, RIGHT }
 
 var facingDirection : Direction
+var moving : bool = false
+@onready var ray = $PlayerMovementRaycast
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -29,4 +34,24 @@ func getInput():
 		Move()
 
 func Move():
-	pass
+	if !moving:
+		match facingDirection:
+			Direction.UP:
+				TryMove(Vector2(0,-TILE_SIZE))
+			Direction.DOWN:
+				TryMove(Vector2(0,TILE_SIZE))
+			Direction.LEFT:
+				TryMove(Vector2(-TILE_SIZE, 0))
+			Direction.RIGHT:
+				TryMove(Vector2(TILE_SIZE,0))
+
+func TryMove(MoveVector : Vector2):
+	ray.target_position = MoveVector
+	ray.force_raycast_update()
+	if !ray.is_colliding():
+		var tween = create_tween()
+		tween.tween_property(self, "position", 
+		position + MoveVector, 1.0/ANIMATION_SPEED).set_trans(Tween.TRANS_SINE)
+		moving = true
+		await tween.finished
+		moving = false
