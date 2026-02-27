b2f391c223c4663cf7e183e9dc55759ad6bc69a3
Ethan Smith <ethanssmith1506@gmail.com>
2026-02-27 21:53:53 +0000

    Neated up code

--- Scenes/MedusaMovement.gd
+++ Scenes/MedusaMovement.gd
@@ -9,16 +9,6 @@ var facingDirection : Direction
 var moving : bool = false
 @onready var ray = $PlayerMovementRaycast
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	getInput()
-
-
 func getInput():
 	if (Input.is_action_just_pressed("MoveUp")):
 		facingDirection = Direction.UP
